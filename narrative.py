69fdac9b1471e30c072f8ed980747954bd98d1fd
JackLamb99 <jacklamb15@yahoo.co.uk>
2024-05-09 20:54:38 +0100

    ADD glade function to open the intro scene upon starting game

--- narrative.py
+++ narrative.py
@@ -13,5 +13,21 @@ however this will reset any progress you have made.
 
 Your journey begins now. Prepare to face the unknown, and may you find the portal
 that will lead you home. Good luck, adventurer. The world is yours to explore.
+"""
 
+glade_text = """
+You find yourself in The Glade.
+
+Golden sunlight filters through a canopy of towering trees. The ground is a lush
+carpet of moss, dotted with colourful wildflowers. A gentle breeze stirs the leaves,
+mingling with the distant birdsong.
+
+An ancient oak stands at the centre, with a message carved into the bark that reads:
+
+	"A warning for the brave of heart,
+	With wisdom only a few impart,
+	Journey through these lands of old,
+	Through caves deep and mountains cold,
+	Where ancient dangers freely roam,
+	You must seek the portals for passage home."
 """
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import narrative as nar
 
+def lst_to_str(lst):
+    return ", ".join(lst)
+
 def main_menu():
     """
     Runs the main menu to start the program, includes the menu_text narrative
@@ -11,6 +14,28 @@ def main_menu():
         print("Invalid input. Please enter 'start' to begin your quest.\n")
         user_input = input("Enter 'start' to begin your quest: ")
 
-    print("Call glade function here")
+    glade()
 
-main_menu()
+def glade():
+    """
+    Runs the Glade scene
+    """
+    directions = ["north", "east", "south", "west"]
+    print(nar.glade_text)
+    print(f"Directions: {lst_to_str(directions)}")
+    user_input = input("Where would you like to go?: ")
+
+    if user_input.lower() == "north":
+        print("Call Mountains desc. function.")
+    elif user_input.lower() == "east":
+        print("Call Caves desc. function.")
+    elif user_input.lower() == "south":
+        print("Call Scorchlands desc. function.")
+    elif user_input.lower() == "west":
+        print("Call Wetlands desc. function.")
+    else:
+        print(f"Invalid direction. Please enter a valid direction: {lst_to_str(directions)}\n")
+        user_input = input("Where would you like to go?: ")
+
+run_game = main_menu()
+run_game
