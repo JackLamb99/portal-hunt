f7cdf30aac877a299f2505b0fe9a3c8533968aef
JackLamb99 <jacklamb15@yahoo.co.uk>
2024-05-16 16:38:31 +0100

    ADD earthshaker function, grid ref. scorchlands-4A

--- scorchlands.py
+++ scorchlands.py
@@ -15,6 +15,54 @@ import general as gen
 import glade
 
 
+def earthshaker():
+    """
+    Checks if the 'earthshaker' item is in the inventory.
+    Runs an 'existing item' scene if true, allows player to 'keep' or 'return'
+    the item.
+    Runs a 'new item' scene if false, allows player to 'take' or 'leave' the
+    item.
+    Grid ref. 'Scorchlands-4A'
+    """
+    directions = ["west"]
+
+    # If the specified item already in the inventory
+    if "earthshaker" in gen.inventory:
+        options = ["keep", "return"]
+
+        print(nar.EARTHSHAKER_EXISTING_ITEM_TEXT)
+        print(f"Options: {gen.lst_to_str(options)}")
+
+        valid_input = gen.get_valid_input(
+            "What would you like to do with this item?: ", options)
+        if valid_input == "keep":
+            print(nar.KEEP_ITEM_TEXT)
+        elif valid_input == "return":
+            gen.inventory.remove("earthshaker")
+            print(nar.RETURN_ITEM_TEXT)
+    else:
+        options = ["take", "leave"]
+
+        print(nar.EARTHSHAKER_NEW_ITEM_TEXT)
+        print(f"Options: {gen.lst_to_str(options)}")
+
+        valid_input = gen.get_valid_input(
+            "What would you like to do with this item?: ", options)
+        if valid_input == "take":
+            gen.inventory.append("earthshaker")
+            print(nar.TAKE_ITEM_TEXT)
+        elif valid_input == "leave":
+            print(nar.LEAVE_ITEM_TEXT)
+
+    print(f"Directions: {gen.lst_to_str(directions)}")
+
+    valid_input = gen.get_valid_input("Where would you like to go?: ",
+                                      directions)
+    if valid_input == "west":
+        gen.clear()
+        scor_crossrd_3a()
+
+
 def scor_crossrd_1c():
     """
     Runs a 'crossroad' scene. Grid ref. 'Scorchlands-1C'
@@ -80,7 +128,7 @@ def scor_crossrd_3a():
         crawler()
     elif valid_input == "east":
         gen.clear()
-        print("Call earthshaker() function")  # TO BE DEFINED
+        earthshaker()
     elif valid_input == "west":
         gen.amend_flee_direction(valid_input)
         gen.clear()
