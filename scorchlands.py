f951b65096f21533f79a645dfeb7288c2c03f112
JackLamb99 <jacklamb15@yahoo.co.uk>
2024-05-16 16:04:47 +0100

    ADD wolves_2c function, grid ref. scorchlands-2C

--- scorchlands.py
+++ scorchlands.py
@@ -15,6 +15,88 @@ import general as gen
 import glade
 
 
+def wolves_2c():
+    """
+    Checks if 'wolves-2c' are alive (True) or dead (False), runs a 'fight' or
+    'flee' scene if alive, runs a 'enemy cleared' scene if dead.
+    Checks if the required item/s are in the 'Inventory' to determine the
+    outcome of the 'fight'.
+    Grid ref. 'Scorchlands-2C'
+    """
+    directions = ["north", "east", "south", "west"]
+
+    # If the specified enemy is 'alive'
+    if gen.enemies["wolves-2c"]:
+        options = ["fight", "flee"]
+
+        print(nar.WOLVES_DESC_TEXT)
+        print(f"Options: {gen.lst_to_str(options)}")
+
+        valid_input = gen.get_valid_input("What would you like to do?: ",
+                                          options)
+        # Fight scene
+        if valid_input == "fight":
+            gen.clear()
+            # If all required items in inventory, victory scene
+            if "sabre" in gen.inventory:
+                gen.enemies["wolves-2c"] = False
+
+                print(nar.WOLVES_VICTORY_TEXT)
+                print(f"Directions: {gen.lst_to_str(directions)}")
+
+                valid_input = gen.get_valid_input(
+                    "Where would you like to go?: ", directions)
+                if valid_input == "north":
+                    gen.clear()
+                    scor_crossrd_2d()
+                elif valid_input == "east":
+                    gen.clear()
+                    scor_crossrd_3c()
+                elif valid_input == "south":
+                    gen.clear()
+                    print("Call scor_crossrd_2b() function")  # TO BE DEFINED
+                elif valid_input == "west":
+                    gen.clear()
+                    print("Call scor_crossrd_1c() function")  # TO BE DEFINED
+            # If required items not in inventory, defeat scene
+            else:
+                print(nar.WOLVES_DEFEAT_TEXT)
+                print("Call end game function")  # TO BE DEFINED
+        # Flee scene
+        elif valid_input == "flee":
+            gen.clear()
+            print(f"You flee back to the {gen.lst_to_str(gen.flee_direction)}")
+
+            if gen.flee_direction == ["north"]:
+                scor_crossrd_2d()
+            elif gen.flee_direction == ["east"]:
+                scor_crossrd_3c()
+            elif gen.flee_direction == ["south"]:
+                print("Call scor_crossrd_2b() function")  # TO BE DEFINED
+            elif gen.flee_direction == ["west"]:
+                print("Call scor_crossrd_1c() function")  # TO BE DEFINED
+
+    # Else if the enemy is already defeated, 'cleared' scene
+    else:
+        print(nar.WOLVES_CLEARED_TEXT)
+        print(f"Directions: {gen.lst_to_str(directions)}")
+
+        valid_input = gen.get_valid_input("Where would you like to go?: ",
+                                          directions)
+        if valid_input == "north":
+            gen.clear()
+            scor_crossrd_2d()
+        elif valid_input == "east":
+            gen.clear()
+            scor_crossrd_3c()
+        elif valid_input == "south":
+            gen.clear()
+            print("Call scor_crossrd_2b() function")  # TO BE DEFINED
+        elif valid_input == "west":
+            gen.clear()
+            print("Call scor_crossrd_1c() function")  # TO BE DEFINED
+
+
 def crawler():
     """
     Checks if the 'crawler' is alive (True) or dead (False), runs a 'fight' or
@@ -189,7 +271,7 @@ def scor_crossrd_2d():
     elif valid_input == "south":
         gen.amend_flee_direction(valid_input)
         gen.clear()
-        print("Call wolves_2c() function")  # TO BE DEFINED
+        wolves_2c()
     elif valid_input == "west":
         gen.clear()
         print("Call sabre() function")  # TO BE DEFINED
@@ -220,7 +302,7 @@ def scor_crossrd_3c():
     elif valid_input == "west":
         gen.amend_flee_direction(valid_input)
         gen.clear()
-        print("Call wolves_2c() function")  # TO BE DEFINED
+        wolves_2c()
 
 
 def scor_crossrd_4d():
