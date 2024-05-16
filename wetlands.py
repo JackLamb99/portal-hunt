4690b13a6a62a85e48f9d648fc94372a0f9404f2
JackLamb99 <jacklamb15@yahoo.co.uk>
2024-05-16 18:35:44 +0100

    ADD trident function, grid ref. wetlands-2A

--- wetlands.py
+++ wetlands.py
@@ -15,6 +15,60 @@ import general as gen
 import glade
 
 
+def trident():
+    """
+    Checks if the 'trident' item is in the inventory.
+    Runs an 'existing item' scene if true, allows player to 'keep' or 'return'
+    the item.
+    Runs a 'new item' scene if false, allows player to 'take' or 'leave' the
+    item.
+    Grid ref. 'Wetlands-2A'
+    """
+    directions = ["north", "east", "west"]
+
+    # If the specified item already in the inventory
+    if "trident" in gen.inventory:
+        options = ["keep", "return"]
+
+        print(nar.TRIDENT_EXISTING_ITEM_TEXT)
+        print(f"Options: {gen.lst_to_str(options)}")
+
+        valid_input = gen.get_valid_input(
+            "What would you like to do with this item?: ", options)
+        if valid_input == "keep":
+            print(nar.KEEP_ITEM_TEXT)
+        elif valid_input == "return":
+            gen.inventory.remove("trident")
+            print(nar.RETURN_ITEM_TEXT)
+    else:
+        options = ["take", "leave"]
+
+        print(nar.TRIDENT_NEW_ITEM_TEXT)
+        print(f"Options: {gen.lst_to_str(options)}")
+
+        valid_input = gen.get_valid_input(
+            "What would you like to do with this item?: ", options)
+        if valid_input == "take":
+            gen.inventory.append("trident")
+            print(nar.TAKE_ITEM_TEXT)
+        elif valid_input == "leave":
+            print(nar.LEAVE_ITEM_TEXT)
+
+    print(f"Directions: {gen.lst_to_str(directions)}")
+
+    valid_input = gen.get_valid_input("Where would you like to go?: ",
+                                      directions)
+    if valid_input == "north":
+        gen.clear()
+        wtl_crossrd_2b()
+    elif valid_input == "east":
+        gen.clear()
+        wtl_crossrd_3a()
+    elif valid_input == "west":
+        gen.clear()
+        print("Call wtl_crossrd_1a() function")  # TO BE DEFINED
+
+
 def wtl_crossrd_2d():
     """
     Runs a 'crossroad' scene. Grid ref. 'Wetlands-2D'
@@ -81,7 +135,7 @@ def wtl_crossrd_2b():
         leeches_3b()
     elif valid_input == "south":
         gen.clear()
-        print("Call trident() function")  # TO BE DEFINED
+        trident()
     elif valid_input == "west":
         gen.amend_flee_direction(valid_input)
         gen.clear()
@@ -105,7 +159,7 @@ def wtl_crossrd_3a():
         leeches_3b()
     elif valid_input == "west":
         gen.clear()
-        print("Call trident() function")  # TO BE DEFINED
+        trident()
 
 
 def leeches_3d():
